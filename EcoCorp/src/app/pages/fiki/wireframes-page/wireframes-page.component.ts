8600f429c7aa61fd870439050f514490275c1d0e
dutualincalin <57103507+dutualincalin@users.noreply.github.com>
2023-12-23 22:47:20 +0200

    Solved pictures not showing - attempt #3

--- EcoCorp/src/app/pages/fiki/wireframes-page/wireframes-page.component.ts
+++ EcoCorp/src/app/pages/fiki/wireframes-page/wireframes-page.component.ts
@@ -10,23 +10,23 @@ export class WireframesPageComponent {
   wireframes = [
     {
       title: 'First page',
-      image: './assets/wireframes/Intro.jpg'
+      image: '/assets/wireframes/Intro.jpg'
     },
     {
       title: 'First page with Login prompt',
-      image: './assets/wireframes/Intro-Login.jpg'
+      image: '/assets/wireframes/Intro-Login.jpg'
     },
     {
       title: 'Sign-up page',
-      image: './assets/wireframes/Sign-up.jpg'
+      image: '/assets/wireframes/Sign-up.jpg'
     },
     {
       title: 'Profile page',
-      image: './assets/wireframes/Profile.jpg'
+      image: '/assets/wireframes/Profile.jpg'
     },
     {
       title: 'Page with products for sale',
-      image: './assets/wireframes/Sales.jpg'
+      image: '/assets/wireframes/Sales.jpg'
     }
   ]
 
