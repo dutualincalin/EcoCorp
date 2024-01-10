fe0f5a9d9d361e8dd224153b6fd08d2f5dd5423f
dutualincalin <57103507+dutualincalin@users.noreply.github.com>
2024-01-11 00:34:21 +0200

    Updated analytics + description + Lead gen clarity

--- EcoCorp/src/app/pages/fiki/analytics-validation-page/analytics-validation-page.component.ts
+++ EcoCorp/src/app/pages/fiki/analytics-validation-page/analytics-validation-page.component.ts
@@ -9,27 +9,59 @@ export class AnalyticsValidationPageComponent {
   report_images: any[] = [
     {
       src: "./assets/fiki-analytics-report/Report_1.png",
-      alt: "First measurement"
+      alt: "New users",
+      text: "The amount of new users had a spike at the end of last year, but then went to a constant of 5 new people " +
+        "in the next month which means that the app in incipient phase brings some interest."
     },
     {
       src: "./assets/fiki-analytics-report/Report_2.png",
-      alt: "Second measurement"
+      alt: "Average Time Management",
+      text: "The average engagement time, it further depicts the user's level of interest which spiked at the end of the last " +
+        "year, went to normal and then started an ascending trend"
     },
     {
       src: "./assets/fiki-analytics-report/Report_3.png",
-      alt: "Third measurement"
+      alt: "New users by default channel group",
+      text: "The metric shows a distribution of the sources from which new users accessed to our website."
     },
     {
       src: "./assets/fiki-analytics-report/Report_4.png",
-      alt: "Forth measurement"
+      alt: "Users by country",
+      text: "The distribution of users by country."
     },
     {
       src: "./assets/fiki-analytics-report/Report_5.png",
-      alt: "Fifth measurement"
+      alt: "User activity over time",
+      text: "The user activity over time shows how much he interacted with the landing page and overall acativity is on " +
+        "an ascending trend"
     },
     {
       src: "./assets/fiki-analytics-report/Report_6.png",
-      alt: "Sixth measurement"
+      alt: "Heat map",
+      text: "The heatmap from the landing page shows that most of the new users will scroll until the second section and " +
+        "then leave the website, but half of the users would scroll until the end of the page, so this might show some " +
+        "level of interest."
+    },
+    {
+      src: "./assets/fiki-analytics-report/Report_7.png",
+      alt: "Heat map",
+      text: "The heatmap from the landing page shows that most of the new users will scroll until the second section and " +
+        "then leave the website, but half of the users would scroll until the end of the page, so this might show some " +
+        "level of interest."
+    },
+    {
+      src: "./assets/fiki-analytics-report/Report_8.png",
+      alt: "Heat map",
+      text: "The heatmap from the landing page shows that most of the new users will scroll until the second section and " +
+        "then leave the website, but half of the users would scroll until the end of the page, so this might show some " +
+        "level of interest."
+    },
+    {
+      src: "./assets/fiki-analytics-report/Report_9.png",
+      alt: "Heat map",
+      text: "The heatmap from the landing page shows that most of the new users will scroll until the second section and " +
+        "then leave the website, but half of the users would scroll until the end of the page, so this might show some " +
+        "level of interest."
     },
   ]
 
